7859d04078f230ca12ebd011f0137dc716c1f960
Rodrigo Soria <Rodrigo Soria@DESKTOP-GC19GEU>
2017-06-08 01:11:05 -0300

    Interfaz grafica lista - Falta agregarle NICS y Ventiladores. (dinamico)

--- src/monitor/Testing.java
+++ src/monitor/Testing.java
@@ -2,12 +2,14 @@ package monitor;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import javax.swing.JFrame;
 import monitor.NICS.INIC;
 import oshi.SystemInfo;
+import ui.UserInterface;
 
 
 public class Testing {
-        public static void main(String[] Args){
+        public static void main(String[] Args) throws InterruptedException{
             
             switch (SystemInfo.getCurrentPlatformEnum()) {
             //Armo los casos para cada OS
@@ -26,7 +28,21 @@ public class Testing {
             }
         }
         
-        private static void test(Monitor m){            
+        private static void test(Monitor m) throws InterruptedException{
+            
+            //Testeando interfaz
+            UserInterface gui = construirGUI(m);
+            gui.setVisible(true);
+            
+            while (true) {
+                //Actualiza los datos constantemente.
+                gui.actualizarDatosSensorYCarga();
+                
+                // pone el poner el hilo actual en pausa
+                // por el tiempo que se necesite en milisegundos
+                Thread.sleep(gui.getFrecuenciaActualizacion()*10); //Hasta 1 segundo de retardo
+            }
+            
             /*
             System.out.println(m.getMicro().toJson());            
             //Tester de Rodrigo soria. -testeando modificado por Mischuk por los JSON
@@ -38,23 +54,30 @@ public class Testing {
             System.out.println(m.getSensores().toJson());
             //Testeando PlacaMadre
             System.out.println(m.getPlacaMadre().toJson());  
-            //Testeando NICS - Uso basico-practico            
+            //Testeando NICS - Uso basico-practico
             //NO BORRAR >:v
             ArrayList<INIC> nics = m.getNetworks().getNics();
-            /*
             for(INIC controlador : nics){
                 System.out.println("Nombre: " + controlador.getNombre());
                 System.out.println("IPv4: " + Arrays.toString(controlador.getIPv4()));
                 System.out.println("MAC: " + controlador.getMacAdress());
             }
-            
             //Testeando NICS - Uso Json:
             for (INIC controlador : nics)
                 System.out.println(controlador.toJson());
-            */            
-               
-            System.out.println(m.toJson());
+            */
+            
+            //System.out.println(m.toJson());
             
             
         }
+        
+        
+    private static UserInterface construirGUI(Monitor monitor) {
+        UserInterface UI = new UserInterface(monitor);
+        UI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        UI.setSize(1024, 600);
+        return UI;
+    }
+        
 }
